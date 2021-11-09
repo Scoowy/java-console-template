4fbd567d5cb0e9d526ab4805d08516e9518bb909
Scoowy <gahonajuanjo@gmail.com>
2021-11-09 12:20:14 -0500

    Beautify the compiler.cmd script

--- compiler.cmd
+++ compiler.cmd
@@ -1,4 +1,16 @@
+@echo off
+
 dir .\src\*.java /s/a:-d/b > sources.txt
+echo [OK] Write sources.txt
+
 rd /s /q build
 mkdir build
+echo [OK] Clean build folder
+
 javac -d build @sources.txt
+echo [OK] Copile project
+
+type nul > file.txt
+echo "Texto a poner ahi" > .\build\runner.cmd
+
+echo [OK] Write runner.cmd
