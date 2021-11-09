e60374f06a169f8b3df591d3fb1b4a184705ae15
Scoowy <gahonajuanjo@gmail.com>
2021-11-09 15:05:56 -0500

    Implemented the creation of the runner.cmd file automatically

--- compiler.cmd
+++ compiler.cmd
@@ -1,16 +1,15 @@
 @echo off
 
+echo [OK] Writing the sources.txt file
 dir .\src\*.java /s/a:-d/b > sources.txt
-echo [OK] Write sources.txt
 
+echo [OK] Clearing the build folder
 rd /s /q build
 mkdir build
-echo [OK] Clean build folder
 
+echo [OK] Compiling the Java project
 javac -d build @sources.txt
-echo [OK] Copile project
 
-type nul > file.txt
-echo "Texto a poner ahi" > .\build\runner.cmd
-
-echo [OK] Write runner.cmd
+echo [OK] Writing the runner.cmd file
+echo @echo off > .\build\runner.cmd
+echo java -cp . consoletest.ConsoleTest >> .\build\runner.cmd
